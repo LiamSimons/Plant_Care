cb29e611c0366c13d6aaff4f8339c34a77fc82e1
liam.simons <liam.simons@student.kuleuven.be>
2019-05-10 14:21:23 +0200

    Real App

---
+++ app/src/main/java/com/example/plantcare/AddPlant.java
@@ -0,0 +1,13 @@
+package com.example.plantcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPlant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_plant);
+    }
+}
