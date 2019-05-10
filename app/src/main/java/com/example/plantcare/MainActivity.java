6523a73421b00c8a0152eafbff379e3db444c755
anton voeten <anton.voeten@student.kuleuven.be>
2019-05-10 15:16:15 +0200

    first intent

--- app/src/main/java/com/example/plantcare/MainActivity.java
+++ app/src/main/java/com/example/plantcare/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.plantcare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToMyPlants(View view) {
+        Intent intent= new Intent(this, MyPlants.class);
+        startActivity(intent);
+    }
+
+    public void goToAdd(View view) {
+        Intent intent = new Intent (this, AddPlant.class);
+        startActivity(intent);
     }
 }
