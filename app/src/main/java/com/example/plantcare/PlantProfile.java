e6ae1cfb133cf57cb4f72fbbfad2f0c13644d51c
liam.simons <liam.simons@student.kuleuven.be>
2019-05-17 11:56:48 +0200

    plant class

--- app/src/main/java/com/example/plantcare/PlantProfile.java
+++ app/src/main/java/com/example/plantcare/PlantProfile.java
@@ -2,12 +2,18 @@ package com.example.plantcare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import classes.Plant;
 
 public class PlantProfile extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_profile);
+        TextView naam = findViewById(R.id.textView2);
+        String str = Plant.getNickname();
+        naam.setText(str);
     }
 }
