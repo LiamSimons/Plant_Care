bdbc5c06bf42f06838b914afcadeb5520435d524
liam.simons <liam.simons@student.kuleuven.be>
2019-05-24 11:02:55 +0200

    Icon en naam verandert en basic recycleview geïmplementeerd

--- app/src/main/java/classes/RecyclerViewAdapter.java
+++ app/src/main/java/classes/RecyclerViewAdapter.java
@@ -1,7 +1,10 @@
 package classes;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -11,43 +14,67 @@ import com.example.plantcare.R;
 
 import java.util.ArrayList;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
-    private static final String TAG = "RecyclerViewAdapter";
+    private ArrayList<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
 
-    private ArrayList<String> nickName = new ArrayList<>();
-    private ArrayList<String> species = new ArrayList<>();
-    private ArrayList<String> plantDate = new ArrayList<>();
-    private ArrayList<String> wateringTime = new ArrayList<>();
-
-
-    public RecyclerViewAdapter(ArrayList<String> nickName{
-        this.nickName = nickName;
+    // data is passed into the constructor
+    public RecyclerViewAdapter(Context context, ArrayList<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
     }
 
-    @NonNull
+    // inflates the row layout from xml when needed
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.layout_list_my_plants, parent, false);
+        return new ViewHolder(view);
     }
 
+    // binds the data to the TextView in each row
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String animal = mData.get(position);
+        holder.myTextView.setText(animal);
     }
 
+    // total number of rows
     @Override
     public int getItemCount() {
-        return 0;
+        return mData.size();
     }
 
-    public class  ViewHolder extends RecyclerView.ViewHolder{
 
-        RelativeLayout parentLayout;
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
 
-        public ViewHolder(@NonNull View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
-            parentLayout = itemView.findViewById(R.id.parent_layout);
+            myTextView = itemView.findViewById(R.id.plantName);
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    public String getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
     }
 }
--- app/src/main/java/com/example/plantcare/MyPlants.java
+++ app/src/main/java/com/example/plantcare/MyPlants.java
@@ -3,10 +3,18 @@ package com.example.plantcare;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
-public class MyPlants extends AppCompatActivity {
+import java.util.ArrayList;
+
+import classes.RecyclerViewAdapter;
+
+public class MyPlants extends AppCompatActivity implements RecyclerViewAdapter.ItemClickListener{
     private String email;
+    private RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +22,26 @@ public class MyPlants extends AppCompatActivity {
         setContentView(R.layout.activity_my_plants);
         Bundle extras = getIntent().getExtras();
         email = extras.getString("EMAIL");
-        
+
+        ArrayList<String> plantNames = new ArrayList<>();
+
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(llm);
+        adapter = new RecyclerViewAdapter(this, plantNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 
     public void goToAdd(View view) {
         Intent intent = new Intent (this, AddPlant.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
